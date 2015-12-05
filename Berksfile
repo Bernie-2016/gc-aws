173cfb23a94f9ecc199366fc52a8b51f93820688
Zach Schneider <zach@schneid.io>
2015-12-04 16:41:47 -0800

    Install postgres and multicorn

---
+++ Berksfile
@@ -0,0 +1,6 @@
+source "https://supermarket.chef.io"
+
+metadata
+
+cookbook 'postgresql', '~> 3.4.24'
+cookbook 'multicorn', '~> 1.0.0'
---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: gc-aws
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'postgresql'
+include_recipe 'multicorn'
