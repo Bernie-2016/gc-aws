f33d8db6ccc3e14b6e24ebf8b3d24c9de99cfcee
Zach Schneider <zach@schneid.io>
2015-12-08 13:33:49 -0600

    Update listen address

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,5 @@ node.default['postgresql']['data_directory']    = "/var/lib/postgresql/#{node['p
 node.default['postgresql']['hba_file']          = "/etc/postgresql/#{node['postgresql']['version']}/main/pg_hba.conf"
 node.default['postgresql']['ident_file']        = "/etc/postgresql/#{node['postgresql']['version']}/main/pg_ident.conf"
 node.default['postgresql']['external_pid_file'] = "/var/run/postgresql/#{node['postgresql']['version']}-main.pid"
+node.default['postgresql']['listen_addresses']  = '*'
 node.default['postgresql']['pg_hba']            = [{type: 'host', db: 'ground_control', user: 'ground_control', addr: '127.0.0.1/32', method: 'trust'}]
