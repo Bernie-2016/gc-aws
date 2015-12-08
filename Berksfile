21a6e5c168e77b8ea13fffc4bcf8f348be68bd71
Zach Schneider <zach@schneid.io>
2015-12-08 15:57:16 -0600

    Add postgis

--- Berksfile
+++ Berksfile
@@ -2,4 +2,5 @@ source "https://supermarket.chef.io"
 
 cookbook 'gc-aws', path: '.'
 cookbook 'postgresql', '~> 0.16.1', git: 'https://github.com/phlipper/chef-postgresql.git'
+cookbook 'postgis', '~> 0.3.0'
 cookbook 'multicorn', '~> 1.0.4'
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,15 +8,15 @@
 #
 
 include_recipe 'postgresql'
-include_recipe 'postgresql::server'
 include_recipe 'multicorn'
+include_recipe 'postgis::default'
 
 postgresql_user 'ground_control' do
   superuser false
   createdb false
   login true
   replication false
-  password begin; node[:deploy]['ground_control']['environment']['PG_PASSWORD']; rescue; 'dev_password'; end
+  password 'password'
 end
 
 postgresql_database 'ground_control' do
@@ -27,5 +27,9 @@ end
 
 # install extensions to database
 postgresql_extension 'multicorn' do
+  database 'ground_control'
+end
+
+postgresql_extension 'postgis' do
   database 'ground_control'
 end
